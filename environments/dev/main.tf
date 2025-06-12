69b1307f126d9b8ad9472b10890c741e0d38ab08
vikyij <vikkyij54@gmail.com>
2025-06-12 15:45:42 +0100

    use terraform to provision vpc with 2 public and private subnets, igw, nat gateway, route table

---
+++ environments/dev/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source = "../../modules/vpc"
+
+  vpc_cidr            = var.vpc_cidr
+  vpc_azs             = var.vpc_azs
+  vpc_public_subnets  = var.vpc_public_subnets
+  vpc_private_subnets = var.vpc_private_subnets
+  enable_nat_gateway  = var.enable_nat_gateway
+  enable_vpn_gateway  = var.enable_vpn_gateway
+  env                 = var.env
+}
