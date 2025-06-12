5ff834beb101ae966f109d6984238d39ef978e60
vikyij <vikkyij54@gmail.com>
2025-06-12 15:47:04 +0100

    define the structure

---
+++ modules/vpc/main.tf
@@ -0,0 +1,23 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = var.vpc_cidr
+
+  azs             = var.vpc_azs
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
+
+  enable_nat_gateway = var.enable_nat_gateway
+  enable_vpn_gateway = var.enable_vpn_gateway
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  single_nat_gateway = true
+  map_public_ip_on_launch = true
+
+  tags = {
+    Terraform = "true"
+    Environment = var.env
+    Project= "my-project"
+  }
+}
