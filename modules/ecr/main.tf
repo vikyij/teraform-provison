9014db6118a8b8082946570217e45e21e439aaf7
vikyij <vikkyij54@gmail.com>
2025-06-16 12:05:17 +0100

    create ecr repo for the different services

---
+++ modules/ecr/main.tf
@@ -0,0 +1,154 @@
+module "ecr_react_frontend" {
+  source = "terraform-aws-modules/ecr/aws"
+  version = "2.4.0"
+
+  repository_name = "react-frontend"
+  
+
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 30 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 30
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Terraform   = "true"
+    Environment = var.env
+  }
+}
+
+module "ecr_fastapi" {
+  source = "terraform-aws-modules/ecr/aws"
+  version = "2.4.0"
+
+  repository_name = "fastapi-api"
+  
+
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 30 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 30
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Terraform   = "true"
+    Environment = var.env
+  }
+}
+
+module "ecr_celery_worker" {
+  source = "terraform-aws-modules/ecr/aws"
+  version = "2.4.0"
+
+  repository_name = "celery-worker"
+  
+
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 30 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 30
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Terraform   = "true"
+    Environment = var.env
+  }
+}
+
+module "ecr_celery_beat" {
+  source = "terraform-aws-modules/ecr/aws"
+  version = "2.4.0"
+
+  repository_name = "celery-beat"
+  
+
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 30 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 30
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Terraform   = "true"
+    Environment = var.env
+  }
+}
+
+module "ecr_data_poller" {
+  source = "terraform-aws-modules/ecr/aws"
+  version = "2.4.0"
+
+  repository_name = "data-poller"
+  
+
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 30 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 30
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Terraform   = "true"
+    Environment = var.env
+  }
+}
